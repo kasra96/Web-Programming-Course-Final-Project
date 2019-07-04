72cfebcd5fea59d77e2940842b518f4aa436a6aa
kasra96 <kasra.darvishi@aut.ac.ir>
2019-07-04 23:29:24 +0430

    Add categories to card

--- web_final_front/src/components/restaurant_card.jsx
+++ web_final_front/src/components/restaurant_card.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "../assets/search_result.css";
+import "../assets/global.css";
 import Stars from "./stars";
 
 class restaurant_card extends Component {
@@ -13,7 +14,19 @@ class restaurant_card extends Component {
           </div>
           <div className="restaurant_info">
             {console.log(this.props.rest_info)}
+            <span className="restaurant_name">{this.props.rest_info.name}</span>
             <Stars average={3.3} />
+            <div>
+              <ul className="categories small_cat_font">
+                {this.props.rest_info.categories.map((c, i) => {
+                  return (
+                    <li key={i}>
+                      <p>{c.persian_name} &bull; </p>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
           </div>
         </div>
         <div className="button_section text-center">
--- web_final_front/src/components/stars.jsx
+++ web_final_front/src/components/stars.jsx
@@ -6,32 +6,26 @@ class Start extends Component {
   state = {};
   initialize(average) {
     var starArr = new Array();
-    for (var i = 0; i < Math.floor(average); i++) {
-      starArr.push(<span className="fa fa-star checked">&nbsp;</span>);
-    }
     if (average !== Math.floor(average)) {
-      starArr.push(
-        <span className="fa fa-star-half-alt halfchecked">&nbsp;</span>
-      );
       for (var i = 0; i < 4 - Math.floor(average); i++) {
-        starArr.push(<span className="far fa-star checked">&nbsp;</span>);
+        starArr.push(<span className="far fa-star">&nbsp;</span>);
       }
+      starArr.push(<span className="fa fa-star-half-alt">&nbsp;</span>);
     } else {
       for (var i = 0; i < 5 - Math.floor(average); i++) {
-        starArr.push(<span className="far fa-star checked">&nbsp;</span>);
+        starArr.push(<span className="far fa-star">&nbsp;</span>);
       }
+    }
+    for (var i = 0; i < Math.floor(average); i++) {
+      starArr.push(<span className="fa fa-star">&nbsp;</span>);
     }
     return starArr;
   }
   render() {
     return (
-      <ul>
+      <ul className="star_list">
         {this.initialize(this.props.average).map((c, i) => {
-          return (
-            <li className="star_list" key={i}>
-              {c}
-            </li>
-          );
+          return <li key={i}>{c}</li>;
         })}
       </ul>
     );
